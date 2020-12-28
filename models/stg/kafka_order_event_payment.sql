7079855297787248106c174785d9634e4be422b3
Kevin Krause <kevin.krause@olo.com>
2020-12-28 16:44:03 +0000

    fail

--- models/stg/kafka_order_event_payment.sql
+++ models/stg/kafka_order_event_payment.sql
@@ -1,5 +1,12 @@
-with final as
-(select order_id, seq_num, payment_type, scheme, description, amount, tip, credit_card, paid_in_advance, load_dttm, action, isupdate, row_id
-from raw.kafka_order_event_payment_strm_v)
+{{ config(
+    materialized='incremental',
+    unique_key="row_id")
+ }}
 
-select * from final;
+with final as (
+    select order_id, seq_num, payment_type, scheme, description, amount, tip, credit_card, paid_in_advance, load_dttm, action, isupdate as is_update, row_id
+    from raw.kafka_order_event_payment_strm_v
+)
+
+select *
+from final
